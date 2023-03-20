cc279e19959f73f503f9b3a75391a30c54fca996
realkalen <bettevans@yahoo.com>
2023-03-20 23:09:19 +0300

    settings configuration

--- website1/settings.py
+++ website1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%pu)z(v7eh-6b89ns2kh!z_*_n#e4f8^-a@=g5onawlhjd#vxw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
