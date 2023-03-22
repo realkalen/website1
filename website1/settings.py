b91b25357d08b7d387ff093fdc5fe654eebcce83
realkalen <bettevans@yahoo.com>
2023-03-22 22:43:57 +0300

    database config

--- website1/settings.py
+++ website1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%pu)z(v7eh-6b89ns2kh!z_*_n#e4f8^-a@=g5onawlhjd#vxw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
