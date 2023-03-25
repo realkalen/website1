a7742d792c04abcd17207a055b74da3890ac3c23
realkalen <bettevans@yahoo.com>
2023-03-25 23:45:02 +0300

    ToDoList id

--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from .models import ToDoList, Item
 # Create your views here.
 
 def index(response, id):
-    return HttpResponse("<h1>%s</h1>" % id)
+    ls = ToDoList.objects.get(id=id)
+    return HttpResponse("<h1>%s</h1>" % ls.name)
 
 
