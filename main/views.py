17a48f3e5d4e3f4ffd2e8ca03de22aa06f005ed0
realkalen <bettevans@yahoo.com>
2023-03-27 23:22:04 +0300

    views update

--- main/views.py
+++ main/views.py
@@ -7,6 +7,6 @@ from .models import ToDoList, Item
 def index(response, name):
     ls = ToDoList.objects.get(name=name)
     item = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, str(item.text)))
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
 
 
