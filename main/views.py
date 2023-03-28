2e677ccc2d660f00dabd5519f96738d87a201182
realkalen <bettevans@yahoo.com>
2023-03-28 23:13:05 +0300

    views.py update

--- main/views.py
+++ main/views.py
@@ -9,6 +9,9 @@ def index(response, id):
     item = ls.item_set.get(id=1)
     return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
 
+def home(response):
+    pass
+
 
 
 
