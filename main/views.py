fc1b717ed48546be31183b87f84f079bde9b70bc
realkalen <bettevans@yahoo.com>
2023-03-29 23:22:57 +0300

    views.py arrangement

--- main/views.py
+++ main/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
 
-# Create your views here.
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
